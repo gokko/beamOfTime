fc8ed9eeb5ee359371b64fb4937aef5569530631
Gottfried <gogo@koschel.org>
2019-11-01 20:46:48 +0100

    fix folder

--- raspi-setup/bot-service.sh
+++ raspi-setup/bot-service.sh
@@ -1,6 +1,6 @@
-sudo cp ~pi/beamoftime/raspi-setup/bot.service /lib/systemd/system
+sudo cp ~pi/beamOfTime/raspi-setup/bot.service /lib/systemd/system
 sudo chmod 644 /lib/systemd/system/bot.service
-sudo chmod +w ~pi/beamoftime/app.py
+sudo chmod +w ~pi/beamOfTime/app.py
 sudo systemctl daemon-reload
 sudo systemctl enable bot.service
 sudo systemctl start bot.service
